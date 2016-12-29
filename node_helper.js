cf8b0e18698082dad43c74ac6be96fece983d5b2
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-12-29 18:42:07 -0300

    remove/add extra lines

--- node_helper.js
+++ node_helper.js
@@ -24,26 +24,25 @@ module.exports = NodeHelper.create({
 		this.setConfig();
 	},
 
-
 	setConfig: function() {
 		this.config = {};
 		this.config.urlApiModule = 'http://beta.magicmirror.builders/api/module';
 		this.config.limit = 20;
 	},
 
-
 	// Override socketNotificationReceived method.
 	socketNotificationReceived: function(notification, payload) {
 	},
-
 
 	// create routes for module manager.
 	// recive request and send response
 	extraRoutes: function() {
 		var self = this;
+
 		this.expressApp.get("/module-manager/modules/availables", function(req, res) {
 			self.getModulesAvailables(req, res);
 		});
+
 		this.expressApp.get("/module-manager/uninstall/:module_name", function(req, res) {
 			if (self.removeModule(req.params.module_name)) {
 				res.send({status: true});
@@ -52,6 +51,7 @@ module.exports = NodeHelper.create({
 				res.send({status: false});
 			}
 		});
+
 	},
 
 	// get modules from API config
@@ -64,13 +64,11 @@ module.exports = NodeHelper.create({
 		request({uri: urlApi, encoding: null, headers: this.getHeaderRequest()}).pipe(res);
 	},
 
-
 	getHeaderRequest: function() {
 		nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
 		headers =  {"User-Agent": "Mozilla/5.0 (Node.js "+ nodeVersion + ") MagicMirror/"  + global.version +  " (https://github.com/MichMich/MagicMirror/)"}
 		return headers;
 	},
-
 
 	// clone repository pass by params into Third modules directory
 	cloneRepository: function(url, name) {
